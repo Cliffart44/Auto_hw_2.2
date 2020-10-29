4903a8844d067396e0c6fbcda26f20ab67494cbf
Cliffart <Cliffart44@gmail.com>
2020-10-29 14:29:12 +0300

    Failing without clicking to calendar

--- src/test/java/PageUiTest.java
+++ src/test/java/PageUiTest.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Test;
-import org.jetbrains.annotations.NotNull;
 import org.openqa.selenium.Keys;
 
 import java.time.LocalDate;
@@ -10,23 +9,16 @@ import static com.codeborne.selenide.Condition.*;
 
 public class PageUiTest {
 
-    @NotNull
-    private String when(boolean trim) {
-        long daysToAdd = 377;
-        if (trim) return LocalDate.now().plusDays(daysToAdd).format(DateTimeFormatter.ofPattern("d"));
-        else return LocalDate.now().plusDays(daysToAdd).format(DateTimeFormatter.ofPattern("dd.MM.uuuu"));
-    }
-
     @Test
     void shouldSubmitRequest() {
         open("http://localhost:9999/");
         $("[data-test-id=city] .input__control").setValue("Москва");
-        $("[data-test-id=date] [placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, when(false));
+        $("[data-test-id=date] [placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.uuuu")));
         $("[data-test-id=name] [name=name]").setValue("Альберт Эйнштейн");
         $("[data-test-id=phone] [name=phone]").setValue("+14318791955");
         $("[data-test-id=agreement]>.checkbox__box").click();
         $("button>.button__content").click();
-        $("[data-test-id=notification]").waitUntil(visible, 15000).shouldHave(exactText("Успешно! Встреча успешно забронирована на " + when(false)));
+        $("[data-test-id=notification]").waitUntil(visible, 15000).shouldHave(exactText("Успешно! Встреча успешно забронирована на " + LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.uuuu"))));
     }
 
     @Test
@@ -34,16 +26,12 @@ public class PageUiTest {
         open("http://localhost:9999/");
         $("[data-test-id=city] .input__control").setValue("Мо");
         $$(".menu-item__control").findBy(text("Москва")).click();
-        $("[data-test-id=date] [placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, when(false), Keys.CONTROL + "A", Keys.DELETE);
-        $("[data-step=\"1\"]").click();
-        $("[data-step=\"-1\"]").click();
-        $("[data-step=\"12\"]").click();
-        $("[data-step=\"-12\"]").click();
-        $$(".calendar__day").findBy(text(when(true))).click();
+        $("[data-test-id=date] [placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.uuuu")), Keys.CONTROL + "A", Keys.DELETE);
+//        $$(".calendar__day").findBy(text(LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("d")))).click();
         $("[data-test-id=name] [name=name]").setValue("Альберт Эйнштейн");
         $("[data-test-id=phone] [name=phone]").setValue("+14318791955");
         $("[data-test-id=agreement]>.checkbox__box").click();
         $("button>.button__content").click();
-        $("[data-test-id=notification]").waitUntil(visible, 15000).shouldHave(exactText("Успешно! Встреча успешно забронирована на " + when(false)));
+        $("[data-test-id=notification]").waitUntil(visible, 15000).shouldHave(exactText("Успешно! Встреча успешно забронирована на " + LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.uuuu"))));
     }
 }
